54d36cff7685760897cbed3524adb899f7c873b0
LiamAttClarke <liam.att.clarke@gmail.com>
2017-07-01 18:31:17 -0400

    Added start scene

---
+++ One/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad(this);
+	}
+
+	public void StartGame() {
+		SceneManager.LoadScene("Main");
+	}
+
+	public void EndGame() {
+		SceneManager.LoadScene("Main");
+	}
+}
